d42cd43fe6e6a407d82be95fcb7164eb44ad3b46
MatouSakura <1134979484@qq.com>
2024-04-13 00:29:33 +0800

    testMessageModule

--- src/test/java/com/example/mq/DiskDataCenterTests.java
+++ src/test/java/com/example/mq/DiskDataCenterTests.java
@@ -1,10 +1,13 @@
 package com.example.mq;
 
+import com.example.mq.common.MqException;
 import com.example.mq.datacenter.DataBaseManager;
 import com.example.mq.datacenter.DiskDataCenter;
 import com.example.mq.datacenter.MessageFileManager;
 import com.example.mq.mqserver.core.Exchange;
 import com.example.mq.mqserver.core.ExchangeType;
+import com.example.mq.mqserver.core.MSGQueue;
+import com.example.mq.mqserver.core.Message;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -64,6 +67,56 @@ public class DiskDataCenterTests {
         //删除一条,此时剩20条
         exchangeList = diskDataCenter.selectAllExchanges();
         Assertions.assertEquals(20,exchangeList.size());
+    }
 
+    @Test
+    public void testMessageModule() throws IOException, MqException, ClassNotFoundException {
+        //测试插入消息,删除消息,从队列中加载消息
+        List<Message> expectedMessageList= new LinkedList<>();
+        MSGQueue msgQueue = createTestQueue();
+        //向队列中插入20条消息,再随便删几条
+        for (int i = 0; i < 20; i++) {
+            Message message = createTestMessage("testMessage" + i);
+            diskDataCenter.sendMessage(msgQueue,message);
+            expectedMessageList.add(message);
+        }
+        //删除前20,删除后16
+        Assertions.assertEquals(20,expectedMessageList.size());
+        diskDataCenter.deleteMessage(msgQueue,expectedMessageList.get(4));
+        diskDataCenter.deleteMessage(msgQueue,expectedMessageList.get(5));
+        diskDataCenter.deleteMessage(msgQueue,expectedMessageList.get(6));
+        diskDataCenter.deleteMessage(msgQueue,expectedMessageList.get(7));
+        expectedMessageList = diskDataCenter.loadAllMessagesFromQueue(testQueueName);
+        Assertions.assertEquals(16,expectedMessageList.size());
+
+        //测试从文件加载消息
+        List<Message> actualMessageList= diskDataCenter.loadAllMessagesFromQueue(testQueueName);
+        Assertions.assertEquals(expectedMessageList.size(),actualMessageList.size());
+
+        for (int i = 0; i < actualMessageList.size(); i++) {
+            Message message = expectedMessageList.get(i);
+            Message curMessage = actualMessageList.get(i);
+            System.out.println("[" + i + "] actualMessage=" + curMessage);
+            System.out.println("[" + i + "] message=" + message);
+            Assertions.assertEquals(message.getMessageId(),curMessage.getMessageId());
+            Assertions.assertEquals(message.getRoutingKey(),curMessage.getRoutingKey());
+            Assertions.assertEquals(message.getDeliverMode(),curMessage.getDeliverMode());
+            Assertions.assertArrayEquals(message.getBody(),curMessage.getBody());
+            Assertions.assertEquals(0x1,curMessage.getIsValid());
+            Assertions.assertEquals(0x1,message.getIsValid());
+        }
+    }
+
+    private MSGQueue createTestQueue() {
+        MSGQueue msgQueue = new MSGQueue();
+        msgQueue.setDurable(true);
+        msgQueue.setName(testQueueName);
+        msgQueue.setExclusive(true);
+        return msgQueue;
+    }
+
+    private Message createTestMessage(String content) {
+        Message message = Message.createMessageWithId("testRoutingKey",null,content.getBytes());
+        return message;
     }
 }
